a8cb07ced74fd8c81fcc8c33e2709275f4197914
DESKTOP-QSP5N3H\배재홍 <zhfxmtkachdt@daum.net>
2020-09-20 05:40:37 +0900

    refactor : [Print] modified

--- src/main/java/Print.java
+++ src/main/java/Print.java
@@ -5,7 +5,7 @@ public class Print {
     private static final String PLEASE_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String HOW_MANY_TIMES_TRY_MESSAGE = "시도할 회수는 몇 회인가요?";
     private static final String IS_WIN = " 이(가) 최종 우승하였습니다.";
-    private static final String NOT_CORRECT_PLEASE_INPUT = "잘못된 입력입니다. 다시 입력해주세요.";
+    private static final String PLEASE_INPUT_AGAIN = "잘못된 입력입니다. 다시 입력해주세요.";
 
     private void printCarName(Car car) {
         System.out.print(car.getName());
@@ -34,7 +34,7 @@ public class Print {
     }
 
     void printPleaseInputAgain() {
-        System.out.println(NOT_CORRECT_PLEASE_INPUT);
+        System.out.println(PLEASE_INPUT_AGAIN);
     }
 
     void printHowManyTimesTry() {
