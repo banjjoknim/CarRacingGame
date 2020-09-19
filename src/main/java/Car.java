56b16cc2f448c8546925b9790bab08ddbe6c0179
DESKTOP-QSP5N3H\배재홍 <zhfxmtkachdt@daum.net>
2020-09-19 20:57:04 +0900

    feature : [Car] method "getInputNames", "setCars" added

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Car {
     private final String name;
     private int position = 0;
+
+    private Scanner scanner = new Scanner(System.in);
 
     public Car(String name) {
         this.name = name;
@@ -14,4 +21,13 @@ public class Car {
         return position;
     }
 
+    String getInputNames() {
+        return scanner.next();
+    }
+
+    List<Car> setCars(String names) {
+        return Arrays.stream(names.split(","))
+            .map(Car::new)
+            .collect(Collectors.toList());
+    }
 }
