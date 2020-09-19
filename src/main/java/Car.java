f09c66baa17ada466034f4fabcfb68cb93ddc1a7
DESKTOP-QSP5N3H\배재홍 <zhfxmtkachdt@daum.net>
2020-09-20 00:16:48 +0900

    refactor : [Car] method "init" added

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
@@ -29,5 +31,17 @@ public class Car {
         return Arrays.stream(names.split(","))
             .map(Car::new)
             .collect(Collectors.toList());
+    }
+
+    Map<Car, Integer> init(List<Car> cars) {
+        Map<Car, Integer> carInfos = new HashMap<>();
+
+        int numberOfCars = cars.size();
+        for (int i = 0; i < numberOfCars; i++) {
+            Car car = cars.get(i);
+            carInfos.put(car, car.getPosition());
+        }
+
+        return carInfos;
     }
 }
