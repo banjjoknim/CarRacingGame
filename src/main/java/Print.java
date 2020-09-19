60c469063538121cd36fdc90a08fc3337228a7ec
DESKTOP-QSP5N3H\배재홍 <zhfxmtkachdt@daum.net>
2020-09-20 00:19:19 +0900

    feature : [Print] method "printWinners" added

--- src/main/java/Print.java
+++ src/main/java/Print.java
@@ -1,9 +1,12 @@
+import java.util.List;
 import java.util.Map;
 
 public class Print {
     private static final String START_GAME_MESSAGE = "자동차 경주 게임을 시작합니다.";
     private static final String PLEASE_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String HOW_MANY_TIMES_TRY_MESSAGE = "시도할 회수는 몇 회인가요?";
+
+    private Operation operation = new Operation();
 
     private void printCarName(Car car) {
         System.out.println(car.getName());
@@ -33,5 +36,10 @@ public class Print {
 
     void printHowManyTimesTry() {
         System.out.println(HOW_MANY_TIMES_TRY_MESSAGE);
+    }
+
+    void printWinners(List<Car> cars, Map<Car, Integer> carInfos, int maxPosition) {
+        String winners = operation.getWinnderNames(cars, carInfos, maxPosition);
+        System.out.println(winners);
     }
 }
