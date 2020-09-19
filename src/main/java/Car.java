83ebcd6e936e088432c4487d36b436b6bac2e951
DESKTOP-QSP5N3H\배재홍 <zhfxmtkachdt@daum.net>
2020-09-20 00:38:47 +0900

    refactor : [Car], [Operation] modified

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,9 +1,4 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Car {
     private final String name;
@@ -21,27 +16,5 @@ public class Car {
 
     public int getPosition() {
         return position;
-    }
-
-    String getInputNames() {
-        return scanner.next();
-    }
-
-    List<Car> setCars(String names) {
-        return Arrays.stream(names.split(","))
-            .map(Car::new)
-            .collect(Collectors.toList());
-    }
-
-    Map<Car, Integer> init(List<Car> cars) {
-        Map<Car, Integer> carInfos = new HashMap<>();
-
-        int numberOfCars = cars.size();
-        for (int i = 0; i < numberOfCars; i++) {
-            Car car = cars.get(i);
-            carInfos.put(car, car.getPosition());
-        }
-
-        return carInfos;
     }
 }
--- src/main/java/Operation.java
+++ src/main/java/Operation.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -6,6 +8,28 @@ import java.util.stream.Collectors;
 public class Operation {
     private Scanner scanner = new Scanner(System.in);
     private Condition condition = new Condition();
+
+    String inputNames() {
+        return scanner.next();
+    }
+
+    List<Car> setCars(String names) {
+        return Arrays.stream(names.split(","))
+            .map(Car::new)
+            .collect(Collectors.toList());
+    }
+
+    Map<Car, Integer> init(List<Car> cars) {
+        Map<Car, Integer> carInfos = new HashMap<>();
+
+        int numberOfCars = cars.size();
+        for (int i = 0; i < numberOfCars; i++) {
+            Car car = cars.get(i);
+            carInfos.put(car, car.getPosition());
+        }
+
+        return carInfos;
+    }
 
     private void drive(Car car, Map<Car, Integer> carInfos) {
         int currentPosition = carInfos.get(car);
