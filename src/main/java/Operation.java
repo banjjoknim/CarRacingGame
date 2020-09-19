328aeb0c09e98cc5ee8ea0b79d3a52e516c4f3b4
DESKTOP-QSP5N3H\배재홍 <zhfxmtkachdt@daum.net>
2020-09-19 20:51:58 +0900

    refactor : [Operation] method "setCars" deleted

--- src/main/java/Operation.java
+++ src/main/java/Operation.java
@@ -1,20 +1,9 @@
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Operation {
-    private Scanner scanner = new Scanner(System.in);
-
-    List<Car> setCars() {
-        String names = scanner.next();
-
-        return Arrays.stream(names.split(","))
-            .map(Car::new)
-            .collect(Collectors.toList());
-    }
 
     Map<Car, Integer> init(List<Car> cars) {
         Map<Car, Integer> carInfos = new HashMap<>();
@@ -27,4 +16,5 @@ public class Operation {
 
         return carInfos;
     }
+
 }
