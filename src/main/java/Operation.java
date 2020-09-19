5eb4dccfa508c1e29f8fd5ae261b1d0f012cabd7
DESKTOP-QSP5N3H\배재홍 <zhfxmtkachdt@daum.net>
2020-09-20 00:16:14 +0900

    refactor : [Operation] method "init" deleted

--- src/main/java/Operation.java
+++ src/main/java/Operation.java
@@ -2,29 +2,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Operation {
     private Scanner scanner = new Scanner(System.in);
     private Condition condition = new Condition();
 
-    Map<Car, Integer> init(List<Car> cars) {
-        Map<Car, Integer> carInfos = new HashMap<>();
-
-        int numberOfCars = cars.size();
-        for (int i = 0; i < numberOfCars; i++) {
-            Car car = cars.get(i);
-            carInfos.put(car, car.getPosition());
-        }
-
-        return carInfos;
-    }
-
-    void drive(Car car, Map<Car, Integer> carInfos) {
+    private void drive(Car car, Map<Car, Integer> carInfos) {
         int currentPosition = carInfos.get(car);
         carInfos.put(car, currentPosition + 1);
     }
 
-    void stop(Car car, Map<Car, Integer> carInfos) {
+    private void stop(Car car, Map<Car, Integer> carInfos) {
         int currentPosition = carInfos.get(car);
         carInfos.put(car, currentPosition);
     }
@@ -39,6 +28,20 @@ public class Operation {
 
     int inputMoveTimes() {
         return scanner.nextInt();
+    }
+
+    int getMaxPosition(List<Car> cars, Map<Car, Integer> carInfos) {
+        return cars.stream()
+            .mapToInt(car -> carInfos.get(car))
+            .max()
+            .getAsInt();
+    }
+
+    String getWinnderNames(List<Car> cars, Map<Car, Integer> carInfos, int maxPosition) {
+        return cars.stream()
+            .filter(car -> carInfos.get(car) == maxPosition)
+            .map(Car::getName)
+            .collect(Collectors.joining(", "));
     }
 
 }
