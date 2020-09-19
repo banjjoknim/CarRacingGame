2304917e028ae76a8aaf5ed7e7e16b3519ec7ae8
DESKTOP-QSP5N3H\배재홍 <zhfxmtkachdt@daum.net>
2020-09-20 00:38:02 +0900

    refactor : [Print] modified

--- src/main/java/Print.java
+++ src/main/java/Print.java
@@ -5,11 +5,12 @@ public class Print {
     private static final String START_GAME_MESSAGE = "자동차 경주 게임을 시작합니다.";
     private static final String PLEASE_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String HOW_MANY_TIMES_TRY_MESSAGE = "시도할 회수는 몇 회인가요?";
+    private static final String IS_WIN = " 이(가) 최종 우승하였습니다.";
 
     private Operation operation = new Operation();
 
     private void printCarName(Car car) {
-        System.out.println(car.getName());
+        System.out.print(car.getName());
     }
 
     private void printCarPosition(int position) {
@@ -40,6 +41,6 @@ public class Print {
 
     void printWinners(List<Car> cars, Map<Car, Integer> carInfos, int maxPosition) {
         String winners = operation.getWinnderNames(cars, carInfos, maxPosition);
-        System.out.println(winners);
+        System.out.println(winners + IS_WIN);
     }
 }
