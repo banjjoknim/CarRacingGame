c4a4b6e3ca633909f113685b57273be40d3b4147
DESKTOP-QSP5N3H\배재홍 <zhfxmtkachdt@daum.net>
2020-09-24 21:22:31 +0900

    refactor : [Racing] modified

--- src/main/java/model/Racing.java
+++ src/main/java/model/Racing.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Racing {
-    private static final boolean WINNER = true;
-    private static final boolean LOSER = false;
 
     public List<Car> setCars(String input) {
         String[] names = input.split(",");
@@ -16,31 +14,26 @@ public class Racing {
     }
 
     public void race(List<Car> cars) {
-        int size = cars.size();
-        for (int i = 0; i < size; i++) {
-            Car car = cars.get(i);
-            car.drive(car.getConditionValue());
-        }
+        cars.stream()
+            .forEach(Car::drive);
     }
 
-    public int getMaxPosition(List<Car> cars) {
-        return cars.stream()
-            .mapToInt(Car::getPosition)
-            .max()
-            .getAsInt();
-    }
-
-    public String getWinners(List<Car> cars, int maxPosition) {
+    public String getWinners(List<Car> cars) {
+        int maxPosition = getMaxPosition(cars);
         return cars.stream()
             .filter(car -> isWinner(car, maxPosition))
             .map(Car::getName)
             .collect(Collectors.joining(", "));
     }
 
+    private int getMaxPosition(List<Car> cars) {
+        return cars.stream()
+            .mapToInt(Car::getPosition)
+            .max()
+            .getAsInt();
+    }
+
     private boolean isWinner(Car car, int maxPosition) {
-        if (maxPosition == car.getPosition()) {
-            return WINNER;
-        }
-        return LOSER;
+        return maxPosition == car.getPosition();
     }
 }
