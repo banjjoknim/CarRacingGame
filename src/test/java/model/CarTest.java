eaa76c98b3ef1a8ffba44e82d30b4372881cf848
DESKTOP-QSP5N3H\배재홍 <zhfxmtkachdt@daum.net>
2020-09-30 16:15:01 +0900

    Test : [CarTest] modified

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -4,58 +4,62 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class CarTest {
 
-    @DisplayName("적절한 이름 테스트")
+    @DisplayName("적절한 이름의 객체 생성 테스트")
     @ParameterizedTest
     @ValueSource(strings = {"asdsd", "sjd", "651"})
     void isCorrectNameTest(String input) {
-        Car car = null;
-        try {
-            car = new Car(input);
-        } catch (Exception e) {
-            System.out.println("잘못된 입력입니다.");
-        }
-        assertThat(car.getName()).isEqualTo(input);
+        assertThatCode(() -> new Car(input))
+            .doesNotThrowAnyException();
     }
 
-    @DisplayName("부적절한 이름 테스트")
+    @DisplayName("부적절한 이름의 객체 생성 테스트")
     @ParameterizedTest
-    @ValueSource(strings = {"  ", "         ", "alskdmas  ", "  123456"})
+    @ValueSource(strings = {"  ", "         ", "al  ", "123456"})
     void isNotCorrectNameTest(String input) {
-        Car car = null;
-        try {
-            car = new Car(input);
-        } catch (Exception e) {
-            assertThat(car).isNull();
-        }
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> new Car(input))
+            .withMessage("부적합한 입력입니다.")
+            .withMessageContaining("부적합")
+            .withNoCause();
     }
 
-    @DisplayName("Car객체와 입력된 이름과의 일치 테스트")
+    @DisplayName("초기화된 Car객체의 올바른 상태값 테스트")
     @ParameterizedTest
     @ValueSource(strings = {"asdsd", "sjd", "651"})
-    void getNameTest(String input) {
+    void newCarTest(String input) {
         Car car = new Car(input);
         assertThat(car.getName()).isEqualTo(input);
-    }
-
-    @DisplayName("Car객체와 초기화된 position 일치 테스트")
-    @ParameterizedTest
-    @ValueSource(strings = {"asdsd", "sjd", "651"})
-    void getPositionTest(String input) {
-        Car car = new Car(input);
         assertThat(car.getPosition()).isEqualTo(0);
     }
 
-    @DisplayName("전진 또는 정지 테스트")
+    @DisplayName("초기화된 Car객체의 positionView 테스트")
     @ParameterizedTest
-    @ValueSource(strings = {"asdsd", "sjd", "651"})
-    void driveTest(String input) {
-        Car car = new Car(input);
-        car.drive();
-        assertThat(car.getPosition()).isBetween(0, 1);
+    @CsvSource(value = {"1,-", "2,--", "3,---", "4,----", "5,-----"})
+    void getPositionViewTest(String input, String positionView) {
+        Car car = new Car("test");
+        assertThat(car.getPositionView(Integer.valueOf(input))).isEqualTo(positionView);
+    }
+
+    @DisplayName("전진 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void driveTest(int input) {
+        Car car = new Car("test");
+        car.drive(input);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("정지 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void stopTest(int input) {
+        Car car = new Car("test");
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 
 }
