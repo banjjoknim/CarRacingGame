3c85e2b0750731b424af1ecc7395013bbb9f6fd4
DESKTOP-QSP5N3H\배재홍 <zhfxmtkachdt@daum.net>
2020-10-12 22:14:11 +0900

    Test :  [Condition] Condition 객체의 conditionValue 유효성 테스트 추가

---
+++ src/test/java/model/ConditionTest.java
@@ -0,0 +1,16 @@
+package model;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ConditionTest {
+
+    @DisplayName("Condition 객체의 랜덤한 ConditionValue 유효성 테스트")
+    @Test
+    void newConditionTest() {
+        assertThat(new Condition().getConditionValue())
+            .isBetween(0, 9);
+    }
+}
