b06120e7d1688570fdcb499c42452c21e80a7e7f
banjjoknim <zhfxmtkachdt@daum.net>
2020-10-12 15:32:05 +0900

    refactor : [GameView] modified

--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -1,13 +1,12 @@
 package view;
 
+import model.Car;
+import model.Cars;
+
 import java.util.Arrays;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-
-import model.Car;
-import model.Cars;
 
 public class GameView {
     private static final String START_GAME_MESSAGE = "자동차 경주 게임을 시작합니다.";
@@ -15,6 +14,8 @@ public class GameView {
     private static final String HOW_MANY_TIMES_TRY_MESSAGE = "시도할 회수는 몇 회인가요?";
     private static final String IS_FINALLY_WIN_MESSAGE = " 이(가) 최종 우승하였습니다.";
     private static final String PLEASE_INPUT_AGAIN_MESSAGE = "잘못된 입력입니다. 다시 입력해주세요.";
+    private static final String CAR_POSITION_VIEW = "-";
+    private static final String IS_NOT_POSITIVE_NUMBER = "시도 횟수는 1이상의 정수여야 합니다.";
 
     private Scanner scanner = new Scanner(System.in);
 
@@ -30,20 +31,26 @@ public class GameView {
         System.out.println(HOW_MANY_TIMES_TRY_MESSAGE);
     }
 
-    public void showPleaseInputAgainMessage() {
+    private void showPleaseInputAgainMessage() {
         System.out.println(PLEASE_INPUT_AGAIN_MESSAGE);
     }
 
-    public void showFinallyWinMessage(String winners) {
-        System.out.println(winners + IS_FINALLY_WIN_MESSAGE);
+    public void showFinallyWinMessage(String winnersName) {
+        System.out.println(winnersName + IS_FINALLY_WIN_MESSAGE);
+    }
+
+    public String getWinnersName(List<Car> winners) {
+        return winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
     }
 
     public Cars inputCars() {
         try {
             String inputNames = scanner.nextLine();
             List<Car> cars = Arrays.stream(inputNames.split(","))
-                .map(Car::new)
-                .collect(Collectors.toList());
+                    .map(Car::new)
+                    .collect(Collectors.toList());
             return new Cars(cars);
         } catch (IllegalArgumentException e) {
             showPleaseInputAgainMessage();
@@ -53,21 +60,42 @@ public class GameView {
 
     public int inputMoveTimes() {
         try {
-            return scanner.nextInt();
-        } catch (InputMismatchException e) {
+            if (!scanner.hasNextInt()) {
+                scanner.next();
+                showPleaseInputAgainMessage();
+                return inputMoveTimes();
+            }
+
+            int moveTimes = scanner.nextInt();
+            if (!isPositiveNumber(moveTimes)) {
+                throw new IllegalArgumentException(IS_NOT_POSITIVE_NUMBER);
+            }
+            return moveTimes;
+        } catch (IllegalArgumentException e) {
             showPleaseInputAgainMessage();
-            scanner = new Scanner(System.in); // Scanner의 버그로 인해 다시 초기화해야 한다.
             return inputMoveTimes();
         }
     }
 
+    private boolean isPositiveNumber(int moveTimes) {
+        return moveTimes > 0;
+    }
+
     public void showCars(Cars cars) {
         cars.getCars().stream()
-            .forEach(this::showCarStatus);
+                .forEach(this::showCarStatus);
         System.out.println();
     }
 
     private void showCarStatus(Car car) {
-        System.out.print(car.getName() + " : " + car.getPositionView(car.getPosition())+"\n");
+        System.out.println(car.getName() + " : " + getPositionView(car.getPosition()));
+    }
+
+    private String getPositionView(int position) {
+        String positionView = "";
+        for (int i = 0; i < position; i++) {
+            positionView += CAR_POSITION_VIEW;
+        }
+        return positionView;
     }
 }
