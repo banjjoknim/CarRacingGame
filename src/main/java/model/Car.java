05192f1b806ad931c048adf447bdb33fba7d65f2
banjjoknim <zhfxmtkachdt@daum.net>
2020-10-12 15:28:18 +0900

    refactor : [Car] modified

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -2,25 +2,22 @@ package model;
 
 public class Car {
     private static final int DRIVE_POSITION = 1;
-    private static final int CONDITION_MIN_VALUE = 0;
-    private static final int CONDITION_MAX_VALUE = 10;
     private static final int CONDITION_GOOD_MIN_VALUE = 4;
     private static final boolean GOOD = true;
     private static final boolean BAD = false;
     private static final int CORRECT_NAME_LENGTH_LIMIT = 5;
     private static final String NOT_SUITABLE_INPUT = "부적합한 입력입니다.";
-    private static final String CAR_POSITION_VIEW = "-";
 
     private final String name;
     private int position = 0;
 
     public Car(String name) {
-        validateName(isCorrectName(name));
+        validateName(name);
         this.name = name;
     }
 
-    private void validateName(boolean isCorrectName) {
-        if (!isCorrectName) {
+    private void validateName(String name) {
+        if (!isCorrectName(name)) {
             throw new IllegalArgumentException(NOT_SUITABLE_INPUT);
         }
     }
@@ -38,29 +35,17 @@ public class Car {
     }
 
     public String getName() {
-        return new Car(name).name;
+        return name;
     }
 
     public int getPosition() {
         return position;
     }
 
-    public String getPositionView(int position) {
-        String positionView = "";
-        for (int i = 0; i < position; i++) {
-            positionView += CAR_POSITION_VIEW;
-        }
-        return positionView;
-    }
-
     public void drive(int conditionValue) {
         if (isGood(conditionValue)) {
             this.position = this.position + DRIVE_POSITION;
         }
-    }
-
-    public int getConditionValue() {
-        return (int) (CONDITION_MIN_VALUE + Math.random() * CONDITION_MAX_VALUE);
     }
 
     private boolean isGood(int conditionValue) {
@@ -70,4 +55,7 @@ public class Car {
         return BAD;
     }
 
+    public boolean isWinner(int maxPosition) {
+        return maxPosition == position;
+    }
 }
