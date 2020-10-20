168816cd37a798e533f17f253951f5d0c4381732
DESKTOP-QSP5N3H\배재홍 <zhfxmtkachdt@daum.net>
2020-10-20 22:03:02 +0900

    refactor : [Condition] 불필요한 상태 값 제거, 메소드 구조 수정하였음.

--- src/main/java/model/Condition.java
+++ src/main/java/model/Condition.java
@@ -4,13 +4,7 @@ public class Condition {
     private static final int CONDITION_MIN_VALUE = 0;
     private static final int CONDITION_MAX_VALUE = 10;
 
-    private int conditionValue;
-
-    public Condition() {
-        this.conditionValue = (int) (CONDITION_MIN_VALUE + Math.random() * CONDITION_MAX_VALUE);
-    }
-
-    public int getConditionValue() {
-        return new Condition().conditionValue;
+    public static int getConditionValue() {
+        return (int) (CONDITION_MIN_VALUE + Math.random() * CONDITION_MAX_VALUE);
     }
 }
